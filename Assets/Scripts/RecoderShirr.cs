2a47fd70fe606a7978b216dd5f8c61047a038151
SubsTheTechnomancer <54163143+SubsTheTechnomancer@users.noreply.github.com>
2020-01-26 20:20:35 +0530

    Complete

--- Assets/Scripts/RecoderShirr.cs
+++ Assets/Scripts/RecoderShirr.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Voice.Unity;
+using Photon.Voice.PUN;
 
 public class RecoderShirr : MonoBehaviour
 {
@@ -9,9 +10,16 @@ public class RecoderShirr : MonoBehaviour
     [SerializeField]
     private GameObject _player;
 
-    private void Start(){
+    [SerializeField]
+    private Recorder recorder;
+    private PhotonVoiceView voiceView;
+
+
+    private void Awake(){
 
         GameObject player = Instantiate(_player);
+        voiceView = player.GetComponent<PhotonVoiceView>();
+        voiceView.RecorderInUse = recorder;
 
     }
 
